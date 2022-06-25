fb4cc95eaaaa1ed4e3be2e65d3e86795a3932b05
Sebastian Clancy <foldit.com@gmail.com>
2022-06-24 18:45:47 -0700

    More refactoring for mongodb

--- database.py
+++ database.py
@@ -7,30 +7,89 @@ Three main collections will be use:
 3. Commander Cards: For each commander, what cards does it run? In what quantities?
 """
 from pymongo import MongoClient
-
+import datetime
+from dateutil import parser
 client = MongoClient()
+db = client['pdhrec']
 
 
 def save_metadata(data):
-    print(data)
-    index = data['publicId']
-    db = client['pdhrec']
+    """
+    Inserts deck metadata into the metadata collection, updating if exists already, otherwise inserting.
+    :param data: deck metadata, a dict of publicId, id, lastUpdatedAtUtc, name, hasPrimer, needsUpdate.
+    :return: None
+    """
+    # print(data)
     col = db['metadata']
-    col.insert_one(data)
-    # print(client.list_database_names())
-    
+    data["_id"] = data['publicId']
+    data['lastUpdatedAtUtc'] = parser.parse(data['lastUpdatedAtUtc']).timestamp()
+    data["needsUpdate"] = True
+    col.update_one({"_id": data["_id"]}, {"$set": data}, upsert=True)
+
+
+def load_metadata(publicId):
+    return db['metadata'].find_one({"_id": publicId})
 
 
 def save_cards(data):
-    index = data['publicId']
+    col = db['decks']
+    info = {'_id': data['publicId'], 'cards': {}, 'commanders': {}}
+    for card in data['mainboard']:
+        if data['mainboard'][card]['card']['legalities']['paupercommander'] != "legal":
+            return False
+        info['cards'][card] = data['mainboard'][card]["quantity"]
 
-def insert_new_commander_data(data):
+    # print(info)
+    for card in data['commanders']:
+        info['commanders'][card] = data['commanders'][card]['quantity']
+        if data['commanders'][card]['card']['legalities']['paupercommander'] == "restricted":
+            pass
+        elif data['commanders'][card]['card']['legalities']['paupercommander'] == "legal":
+            if data['commanders'][card]['card']['rarity'] == "uncommon" and data['commanders'][card]['card'][
+                'type'] == "2":
+                pass
+            else:
+                return False
+        else:
+            return False
+    mainboard = 0
+    for card in info['cards']:
+        mainboard += info['cards'][card]
+    for card in info['commanders']:
+        mainboard += info['commanders'][card]
+    if mainboard != 100:
+        return False
+    print("Inserting cards..")
+    col.update_one({'_id': info['_id']}, {"$set": info}, upsert=True)
+    return True
+
+
+def load_cards(publicId):
     pass
 
-def update_commander_data(old_data, new_data):
-    pass
 
+# def insert_new_commander_data(data):
+#     pass
+#
+# def update_commander_data(old_data, new_data):
+#     pass
 
+def get_ids_need_update():
+    col = db['metadata']
+    return [x['_id'] for x in col.find({"needsUpdate": True})]
+
+def get_newest_metadata():
+    col = db['metadata']
+    col.find_one()
+    return [x for x in db['metadata'].find().sort("lastUpdatedAtUtc", -1).limit(1)][0]
+
+def get_all_metadata():
+    return db['metadata'].find({})
+
+def get_commander_aggregate():
+    col = db['decks']
+    result = col.aggregate()
+    print(result)
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,13 +1,59 @@
-from database import save_metadata
-from moxfield import get_new_decks
+import time
+
+from dateutil import parser
+
+from database import save_metadata, load_metadata, save_cards, get_ids_need_update, get_all_metadata, get_newest_metadata
+from moxfield import get_deck_data, get_new_decks
+
+def smart_update():
+    data = get_all_metadata()
+    new_decks = get_new_decks()
+    last_time_update = get_newest_metadata()['lastUpdatedAtUtc']
+    newest_deck = new_decks[0]  # Information is received in sorted order.
+    # Check if the newest deck is newer than the current newest:
+    print("Comparing Times")
+    newest_time = parser.parse(newest_deck['lastUpdatedAtUtc']).timestamp()
+    if last_time_update < newest_time:
+        print("Updating")
+        # Iterate backwards over the new decks, incrementing page count until we pass the right place
+        time_to_reach = last_time_update
+        last_deck_time = newest_time
+        page = 1
+        while time_to_reach < last_deck_time:
+            for deck in new_decks:
+                print(f"Saving deck {deck['name']}")
+                save_metadata({
+                    "id": deck['id'],
+                    "publicId": deck['publicId'],
+                    "hasPrimer": deck['hasPrimer'],
+                    "name": deck['name'],
+                    "lastUpdatedAtUtc": deck['lastUpdatedAtUtc'],
+                })
+                last_deck_time = parser.parse(deck['lastUpdatedAtUtc']).timestamp()
+                if time_to_reach >= last_deck_time:
+                    break  # Reached exit conditions
+            page += 1
+            time.sleep(1)
+            new_decks = get_new_decks(page)
+    else:
+        print("No new decks")
+
+
 if __name__ == "__main__":
-    values = get_new_decks()[0]
-    print(values)
-    save_metadata({
-        "id": values['id'],
-        "publicId": values['publicId'],
-        "hasPrimer": values['hasPrimer'],
-        "name": values['name'],
-        "lastUpdatedAtUtc": values['lastUpdatedAtUtc'],
-    })
-    
+    # Check if there's anything in the database.  If not, add one so we can smart update.
+    if len([x for x in get_all_metadata()]) == 0:
+        deck = get_new_decks(20)[-1]
+        save_metadata({
+            "id": deck['id'],
+            "publicId": deck['publicId'],
+            "hasPrimer": deck['hasPrimer'],
+            "name": deck['name'],
+            "lastUpdatedAtUtc": deck['lastUpdatedAtUtc'],
+        })
+    smart_update()
+    for public_id in get_ids_need_update():
+        card_data = get_deck_data(public_id)
+        save_cards(card_data)
+        data = load_metadata(public_id)
+        data['needsUpdate'] = False
+        save_metadata(data)
