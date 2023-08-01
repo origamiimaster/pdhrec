c72fa52c4a0f2ae39840c305668d0dca6bc5accd
Isaiah Hazelwood <isaiah.hazelwood@mail.utoronto.ca>
2023-08-01 17:14:32 -0400

    Fix normalize_cardnames datatype

--- backend/utils.py
+++ backend/utils.py
@@ -2,7 +2,7 @@ from typing import List
 
 
 def normalize_cardnames(cards: List[str]) -> List[str]:
-    return map(normalize_cardname, cards)
+    return list(map(normalize_cardname, cards))
 
 
 def normalize_cardname(cardname: str, sep="-"):
