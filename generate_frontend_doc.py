6eb676996519bab2c9a750b409338f49068df4b2
origamiimaster <foldit.com@gmail.com>
2023-08-01 21:15:43 -0400

    Fix frontend

--- generate_frontend_doc.py
+++ generate_frontend_doc.py
@@ -4,7 +4,6 @@ from backend.database import Database
 from backend.utils import normalize_cardnames
 from backend.update import perform_update, get_latest_bulk_file
 
-
 if __name__ == "__main__":
     # Create a connection to the database
     with open("server-token.json") as f:
@@ -12,7 +11,7 @@ if __name__ == "__main__":
     database = Database(connection_string)
 
     # Load all cards from bulk json in database
-    cards_path = get_latest_bulk_file(database, directory="scryfall_data")
+    cards_path = get_latest_bulk_file(directory="scryfall_data")
     with open(cards_path, "r") as f:
         all_cards = json.load(f)
 
@@ -21,6 +20,8 @@ if __name__ == "__main__":
     lands = set()
     double_faces = set()
     for card in all_cards:
+        if "type_line" not in card:
+            continue
         if "Land" in card['type_line']:
             lands.add(card['name'])
         try:
@@ -37,7 +38,7 @@ if __name__ == "__main__":
             else:
                 image_lookup[card["name"]] = card["image_uris"]["large"]
         # TODO: More robust error handling
-        except KeyError: # Why isn't this handled with other exceptions?
+        except KeyError:  # Why isn't this handled with other exceptions?
             continue
         except Exception as e:
             print(e)
@@ -45,16 +46,16 @@ if __name__ == "__main__":
             continue
 
     # Commit updates to the database
-    print("Updating datasbase")
+    print("Updating database")
     perform_update(database)
 
     # Generate a list of the commanders / commander pairs, along with their 
     # image urls and cleaned names
-    commander_data = {tuple(deck['commanders']) 
+    commander_data = {tuple(deck['commanders'])
                       for deck in database.decks.aggregate(pipeline=[
-                          {"$match": {"isLegal": True}}, 
-                          {"$project": {"_id": 0, "commanders": 1}}
-                          ])}
+            {"$match": {"isLegal": True}},
+            {"$project": {"_id": 0, "commanders": 1}}
+        ])}
     # Ensure commanders are unique (sort pairs)
     commander_data = list(set(tuple(sorted(x)) for x in commander_data))
 
@@ -69,11 +70,11 @@ if __name__ == "__main__":
                 urls.append(url)
         # print(urls)
         new_commander_data.append({"commanders": commanders, "urls": urls})
-    
+
     # Record all other commander statistics
     all_synergy_scores, popularity_scores, commander_counts, \
         color_popularity = get_all_scores(database)
-    processed = 0 # Number of commanders processed
+    processed = 0  # Number of commanders processed
     new_commander_names = []
     for commander in new_commander_data:
         print(f"Updating: {commander}")
@@ -82,18 +83,16 @@ if __name__ == "__main__":
         # Store commander count
         commander["count"] = commander_counts[tuple(commander['commanders'])]
         # Parse commanderstring (for database use)
-        if (len(commander["commanders"]) == 1 
-            and commander["commanders"][0] in double_faces): # Handle DFCs
+        if (len(commander["commanders"]) == 1
+                and commander["commanders"][0] in double_faces):  # Handle DFCs
             print(commander["commanders"][0])
             commander["urls"] = image_lookup[commander["commanders"][0]]
-            commander["commanders"] = commander["commanders"][0].split(" // ")
-            commander['commanderstring'] = "--".join(normalize_cardnames(commander['commanders']))
-        else: # Handle single-faced and partner commanders
-            commander['commanderstring'] = "-".join(sorted(normalize_cardnames(commander['commanders'])))
-    
-    # Save commander names to file
-    with open("frontend/commandernames.json", "w") as f:
-        json.dump(new_commander_names, f)
+            # commander["commanders"] = commander["commanders"][0].split(" // ")
+            commander['commanderstring'] = "--".join(
+                normalize_cardnames(commander['commanders']))
+        else:  # Handle single-faced and partner commanders
+            commander['commanderstring'] = "-".join(
+                sorted(normalize_cardnames(commander['commanders'])))
 
         # Store synergy scores
         # TODO: Convert item['commanders'] to a hashable type?
@@ -102,17 +101,30 @@ if __name__ == "__main__":
         for card in synergy_scores:
             if card in image_lookup:
                 card_image = image_lookup[card]
-            else: 
+            else:
                 raise Exception(f"{card} not in image_lookup")
             card_info = [card, synergy_scores[card], card_image]
             commander['carddata'].append(card_info)
         # Sort cards by decreasing synergy scores
         commander['carddata'].sort(key=lambda x: -x[1])
 
+        # TODO: Fix this, as a duplicate for the code at the top
+        if (len(commander["commanders"]) == 1
+                and commander["commanders"][0] in double_faces):  # Handle DFCs
+            print(commander["commanders"][0])
+            commander["urls"] = image_lookup[commander["commanders"][0]]
+            commander["commanders"] = commander["commanders"][0].split(" // ")
+            commander['commanderstring'] = "--".join(normalize_cardnames(commander['commanders']))
+
+
         # Update user on processing status
         print(commander['commanderstring'])
         processed += 1
         print(f"{processed} / {len(new_commander_data)}")
+
+    # Save commander names to file
+    with open("frontend/commandernames.json", "w") as f:
+        json.dump(new_commander_names, f)
 
     # Sort commanders by decreasing count and save to file
     new_commander_data.sort(key=lambda x: -x["count"])
@@ -122,13 +134,13 @@ if __name__ == "__main__":
     # Store color popularity (staple) information
     new_color_popularity = []
     for cardname in color_popularity:
-        if cardname in lands: # Skip lands
+        if cardname in lands:  # Skip lands
             continue
         try:
-            commander = [cardname, color_popularity[cardname][0], 
+            commander = [cardname, color_popularity[cardname][0],
                          image_lookup[cardname], color_popularity[cardname][1]]
             new_color_popularity.append(commander)
-        except Exception as e: # TODO: More robust?
+        except Exception as e:  # TODO: More robust?
             print(e)
             print(cardname)
     # Save color popularity to file
